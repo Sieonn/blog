a13865541135ab567939e4b1dbda20fbef3bb0da
sieon <sieon.eve@gmail.com>
2025-07-14 00:08:44 +0900

    chore: 헤더 위치 잡기

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <div>
+          {/* <img src={logo} alt="logo" /> */}
+          <h3>블로그 이름</h3>
+        </div>
+        <div>
+          <div>
+            <div>home</div>
+          </div>
+          <div>
+            <div>home</div>
+          </div>
+          <div>
+            <div>home</div>
+          </div>
+        </div>
+        <div>
+          <div>searchbar</div>
+        </div>
+      </div>
+    </header>
+  );
+}
---
+++ src/page/main.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/Header";
+function Main() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <div>
+          <div>
+            <div></div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Main;
